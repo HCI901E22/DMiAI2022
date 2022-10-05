98056ee0284f76498e6142fcdd30c03265b4dd4b
Lasse Jensen <Lasseon.Jensen@gmail.com>
2022-10-05 10:30:27 +0200

    Request DTO setup

--- robot-robbers/run_game.py
+++ robot-robbers/run_game.py
@@ -1,20 +1,46 @@
-from typing_extensions import Self
 from game.environment import RobotRobbersEnv
+from models.dtos import RobotRobbersPredictRequestDto
 import router
+
 
 env = RobotRobbersEnv()
 env.reset(42)
 env.render()
 sample = env.observation_space.sample()
 
-print('Number of states: {}'.format(env.observation_space))
-print('Number of actions: {}'.format(env.action_space))
+# My variables
+moveStart = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+firstRun = True
+predictDTO = 0
+
+current_key = None
 
 while True:
-    move = router.predict(env._get_observation())
+    if firstRun:
+        state, reward, is_done, info = env.step(moveStart)
+        firstRun = False
+    else:
+        moveDTO = router.predict(predictDTO)
+        state, reward, is_done, info = env.step(moveDTO.moves)
 
-    state, reward, is_done, info = env.step([
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-    ])
+    predict_data = {
+        'state': state.tolist(),
+        'reward': reward,
+        'is_terminal': is_done,
+        'total_reward': info['total_reward'],
+        'game_ticks': info['game_ticks']
+    }
 
+    predictDTO = RobotRobbersPredictRequestDto(**predict_data)
+
+    # If you want to render the game as it runs, it's recommended to
+    # run this script locally:
+    #
+    # ```bash
+    # python3 -m venv .venv
+    # source .venv/bin/activate
+    # pip install -r requirements.txt
+    # python run_game.py
+    # ```
+    #
     env.render()
