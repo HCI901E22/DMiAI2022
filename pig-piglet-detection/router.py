e82bd039875be958469a331bbc92bb20617b8319
Adrian Plesner <adrian.plesner@gmail.com>
2022-10-06 15:48:32 +0200

    pigs int as string

--- pig-piglet-detection/router.py
+++ pig-piglet-detection/router.py
@@ -29,7 +29,7 @@ def predict_endpoint(request: PredictRequestDto):
 def decode_request(request: PredictRequestDto) -> np.ndarray:
     encoded_img: str = request.img
     im = Image.open(BytesIO(base64.b64decode(encoded_img)))
-    path = "image" + random.randint(0,10000) + ".png"
+    path = "image" + str(random.randint(0,10000)) + ".png"
     im.save(path, 'PNG')
     np_img = np.fromstring(base64.b64decode(encoded_img), np.uint8)
     return cv2.imdecode(np_img, cv2.IMREAD_ANYCOLOR)
