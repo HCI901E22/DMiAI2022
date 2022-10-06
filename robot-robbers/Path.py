5965dfa9b94e82ecb633d1d7a81aa0561ec4419b
Oliversrensen <Oliverssorensen@gmail.com>
2022-10-06 21:48:43 +0200

    It somewhat works

--- robot-robbers/Path.py
+++ robot-robbers/Path.py
@@ -8,10 +8,22 @@ def MakeMatrix(fromTile, toTile, state):
     matrix = np.ones((128, 128))
     obstacles = [(x, y, w, h)
                  for (x, y, w, h) in state[4] if x >= 0 and y >= 0]
-    for x in obstacles:
-        for h in range(x[3]):
-            for w in range(x[2]):
-                matrix[x[0] - w][x[1] - h] = 0
+    #for x in obstacles:
+    #    for h in range(x[3]):
+    #        for w in range(x[2]):
+    #            matrix[x[0] + w][x[1] + h] = 0
+
+    for cx in range(0, 127):
+        for cy in range(0, 127):
+            for x, y, w, h in obstacles:
+                if cx >= x and cx <= x + w and cy >= y and cy <= y + h:
+                    matrix[cy][cx] = 0
+
+    for scrooge in state[1]:
+        if(scrooge[0] < 118 and scrooge[1] < 118 and scrooge[1] > 10 and scrooge[0] > 10):
+            for i in range(20):
+                for j in range(20):
+                    matrix[scrooge[1]  - 10 + i][scrooge[0]  - 10 + j] = 0
 
     grid = Grid(matrix=matrix)
 
@@ -20,5 +32,8 @@ def MakeMatrix(fromTile, toTile, state):
 
     finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
     path, runs = finder.find_path(start, end, grid)
-    print(grid.grid_str(path=path, start=start, end=end))
+            
+
+    #print(path)
+    #print(grid.grid_str(path=path, start=start, end=end))
     return path
--- robot-robbers/router.py
+++ robot-robbers/router.py
@@ -3,6 +3,7 @@ import numpy as np
 from fastapi import APIRouter
 from models.dtos import RobotRobbersPredictResponseDto, RobotRobbersPredictRequestDto
 import Path
+import math
 
 router = APIRouter()
 
@@ -18,36 +19,64 @@ def predict(request: RobotRobbersPredictRequestDto):
     dropspots = [(x, y, w, h)
                  for (x, y, w, h) in request.state[3] if x >= 0 and y >= 0]
     obstacles = request.state[4]
-    print(request.total_reward)
-    # Your moves go here!
-    path = []
-    if (path == []):
-        if (request.state[5][0][0] > 0):
+        
+    moves = []
+    #Make path towards cash and then deposit
+    for x in range(5):
+        if (request.state[5][x][0] > 0):
             path = Path.MakeMatrix(
-                (robots[0][0], robots[0][1]),
-                (dropspots[0][0], dropspots[0][1]), request.state)
+                roboPos(robots, x),
+                closestDeposit(dropspots, roboPos(robots, x), request.state[1]), request.state)
         else:
             path = Path.MakeMatrix(
-                (robots[0][0], robots[0][1]),
-                (cashbags[0][0], cashbags[0][1]), request.state)
+                roboPos(robots, x),
+                closestBag(cashbags, roboPos(robots, x), request.state[1]), request.state)
+        moves += doMove((robots[x][0], robots[x][1]), path)
 
     n_robbers = 5
-    moves = doMove((robots[0][0], robots[0][1]), path)
-    # print(moves)
-
+    
     return RobotRobbersPredictResponseDto(
         moves=moves
     )
 
+def roboPos(robots, robotNum):
+    return (robots[robotNum][0], robots[robotNum][1])
 
 def ItemPos(idIndex, itemNum, state):
     return (state[idIndex][itemNum][0], state[idIndex][itemNum][1])
 
+def closestBag(bags, robotPos, scrooges):
+    dist = 100000
+    pos = (0,0)
+    for x in bags:
+        if(x[0] == -1):
+            continue
+        if(math.dist(robotPos, (x[0], x[1])) < dist and checkScroogeNearby((x[0], x[1]), scrooges)):
+            pos = (x[0], x[1])
+            dist = math.dist(robotPos, (x[0], x[1]))
+    return pos
+
+def closestDeposit(depos, robotPos, scrooges):
+    dist = 100000
+    pos = (0,0)
+    for x in depos:
+        if(math.dist(robotPos, (x[0], x[1])) < dist and checkScroogeNearby((x[0], x[1]), scrooges)):
+            pos = (x[0], x[1])
+            dist = math.dist(robotPos, (x[0], x[1]))
+    return pos
+
+def checkScroogeNearby(item, scrooges):
+    for x in range(len(scrooges)):
+        if(math.dist(item, (scrooges[x][0], scrooges[x][1])) < 20):
+            return False
+    return True    
+    
 
 def doMove(robotPos, path):
     move = []
-    #print("RobotPos: {}", robotPos)
-    #print("path: {}", path[0])
+    #Movement in X
+    if(len(path) == 0):
+        return [0, 0]
     if (robotPos[0] > path[1][0]):
         move += [-1]
     elif (robotPos[0] < path[1][0]):
@@ -55,11 +84,11 @@ def doMove(robotPos, path):
     elif (robotPos[0] == path[1][0]):
         move += [0]
 
+    #Movement in Y
     if (robotPos[1] > path[1][1]):
         move += [-1]
     elif (robotPos[1] < path[1][1]):
         move += [1]
     elif (robotPos[1] == path[1][1]):
         move += [0]
-    move += [0, 0, 0, 0, 0, 0, 0, 0]
     return move
