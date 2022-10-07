1cb7cf315cd1d5e0b61e810e922e9022955db519
beef PC <adrian.plesner@gmail.com>
2022-10-07 16:55:52 +0200

    nu virker det

--- pig-piglet-detection/router.py
+++ pig-piglet-detection/router.py
@@ -10,11 +10,11 @@ from PIL import Image
 from io import BytesIO
 
 import os
-from keras-retinanet-main.keras_retinanet.models import load_model
+
 
 
 router = APIRouter()
-
+model = None
 
 
 @router.post('/predict', response_model=PredictResponseDto)
@@ -28,11 +28,18 @@ def predict_endpoint(request: PredictRequestDto):
 
     return response
 
+@router.get('/load')
+def load_model():
+    import os
+    from keras_retinanet_main.keras_retinanet.models import load_model
+    model = load_model('snapshots/mymodelv02.h5', backbone_name = 'resnet101')
+    return "Model loded"
+
 
 def decode_request(request: PredictRequestDto) -> np.ndarray:
     encoded_img: str = request.img
     im = Image.open(BytesIO(base64.b64decode(encoded_img)))
-    #path = "eval-images/image" + str(random.randint(0,10000)) + ".png"
+    #path = "eval-images/reeee" + str(random.randint(0,10000)) + ".png"
     #im.save(path, 'PNG')
     np_img = np.fromstring(base64.b64decode(encoded_img), np.uint8)
     return cv2.imdecode(np_img, cv2.IMREAD_ANYCOLOR)
@@ -42,32 +49,37 @@ def predict(img: np.ndarray) -> List[BoundingBoxClassification]:
     logger.info(f'Recieved image: {img.shape}')
     
     import os
-    from keras_retinanet.models import load_model
-
+    from keras_retinanet_main.keras_retinanet.models import load_model
+    model = load_model('snapshots/mymodelv02.h5', backbone_name = 'resnet101')
     # load retinanet model
-    result = []
-    model = load_model(r'C:\Users\lasse\DMiAI2022\pig-piglet-detection\snapshots\mymodel.h5', backbone_name = 'resnet101')
+    result: List[BoundingBoxClassification] = []
     
-    boxes, scores, labels = model.predict(np.expand_dims(image, axis=0))
     
-
+    boxes, scores, labels = model.predict(np.expand_dims(img, axis=0))
+    count = 0
     for box, score, label in zip(boxes[0], scores[0], labels[0]):
+        print("here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(box)
+        print(score)
         # scores are sorted so we can break
         count = count + 1
         if score < 0.5:
+            print("breaking after " + str(count))
             break
-        b = BoundingBoxClassification(class_id = label, min_x = boxes[0], min_y = boxes[1], max_x = boxes[2], max_y = boxes[3], confidence = score)
+        b = BoundingBoxClassification(class_id=label, min_x=box[0]/img.shape[0], min_y=box[1]/img.shape[1], max_x=box[2]/img.shape[0], max_y=box[3]/img.shape[1], confidence=score)
+        print(b)
         result.append(b)
+    print(result)
     return result
 
 
     
-    bounding_boxes: List[BoundingBoxClassification] = []
-    for _ in range(random.randint(0, 9)):
-        bounding_box: BoundingBoxClassification = get_dummy_box()
-        bounding_boxes.append(bounding_box)
-        logger.info(bounding_box)
-    return bounding_boxes
+    #bounding_boxes: List[BoundingBoxClassification] = []
+    #for _ in range(random.randint(0, 9)):
+    #    bounding_box: BoundingBoxClassification = get_dummy_box()
+    #    bounding_boxes.append(bounding_box)
+    #    logger.info(bounding_box)
+    #return bounding_boxes
 
 
 def get_dummy_box() -> BoundingBoxClassification:
