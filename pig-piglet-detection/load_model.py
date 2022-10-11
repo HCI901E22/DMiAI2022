10b7d8543ca10244e2e0007ea9457f57e2398b99
Lasse Harde <lharde18@student.aau.dk>
2022-10-11 09:13:19 +0200

    retinanet

--- pig-piglet-detection/load_model.py
+++ pig-piglet-detection/load_model.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://kurtskammerater.westeurope.cloudapp.azure.com:4545/load"
 
-data = {'path': "snapshots/model50.h5"}
+data = {'path': "snapshots/model2.h5"}
 
 res = requests.post(url, json=data)
 
